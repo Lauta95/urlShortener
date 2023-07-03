6e383020617efb69b7579916de55a0c22bbfebb7
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-07-03 20:55:27 -0300

    instalacion de mongodb

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,16 +12,17 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/shorturl', (req, res) =>{
-  console.log('test');
+app.get('/api/shorturl', (req, res) => {
+
+
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
