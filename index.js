cc77ce18e250f2820430e4424d64a097ef19b14c
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-07-04 23:12:59 -0300

    req body test

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function (req, res) {
+  console.log(req.body);
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
